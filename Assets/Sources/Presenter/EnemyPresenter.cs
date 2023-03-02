9e8b33eb46f17190831f72c1df7eafd569308a35
Alexander <71867706+Fosf0r4@users.noreply.github.com>
2023-03-02 16:47:51 +0300

    Initial commit

---
+++ Assets/Sources/Presenter/EnemyPresenter.cs
@@ -0,0 +1,13 @@
+﻿using Asteroids.Model;
+using UnityEngine;
+
+public class EnemyPresenter : Presenter
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            DestroyCompose();
+        }
+    }
+}
---
+++ Assets/Sources/Presenter/ShipPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class ShipPresenter : Presenter
+{
+    private Root _init;
+
+    public uint ShipHealth = 3;
+
+    public void Init(Root init)
+    {
+        _init = init;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _init.OnHit(1);
+
+            if (ShipHealth == 0)
+            {
+                _init.GameOver();
+            }
+        }
+    }
+}
